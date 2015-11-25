d584ced9c888f403d84e0788053f3a3bd98dde40
LZ-Luzhuo <luzhuo@vip.qq.com>
2015-11-25 23:27:08 +0800

    add method to AppUtil.java.

--- CodeBase/utils/AppUtil.java
+++ CodeBase/utils/AppUtil.java
@@ -1,16 +1,29 @@
 package com.example.appdemo.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.example.appdemo.R;
+
+import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.app.ActivityManager.MemoryInfo;
+import android.app.ActivityManager.RunningAppProcessInfo;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
 
 /**
  * =================================================
@@ -113,5 +126,197 @@ public class AppUtil {
 		public String packname;
 		public boolean inRom; //安装位置:true手机内存;falseSD卡
 		public boolean userApp; //用户应用:true用户应用;false系统
+	}
+	
+	/**
+	 * 打开一个应用程序
+	 * @param context
+	 * @param packName 包名("com.example.demo") 
+	 * @return 能开启true/不能开启false
+	 */
+	public static boolean startApp(Context context,String packName) {
+		PackageManager pm = context.getPackageManager();
+		Intent intent = pm.getLaunchIntentForPackage(packName);
+		if (intent != null) {
+			context.startActivity(intent);
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	/**
+	 * 判断一个应用程序是否可以启动
+	 * @param context
+	 * @param packName 包名("com.example.demo") 
+	 * @return 能开启true/不能开启false
+	 */
+	public static boolean isStartApp(Context context,String packName) {
+		PackageManager pm = context.getPackageManager();
+		Intent intent = pm.getLaunchIntentForPackage(packName);
+		return intent != null ? true : false;
+	}
+	
+	/**
+	 * 获取所有可以启动的app
+	 * @param context
+	 * @return ResolveInfo的集合(包名:resolveInfo.activityInfo.packageName)
+	 */
+	public static List<ResolveInfo> getStartApp(Context context){
+		PackageManager pm = context.getPackageManager();
+		 Intent intent = new Intent();
+		 intent.setAction("android.intent.action.MAIN");
+		 intent.addCategory("android.intent.category.LAUNCHER");
+		 List<ResolveInfo> infos = pm.queryIntentActivities(intent, PackageManager.GET_INTENT_FILTERS); //查询出所有具有启动能力的activity。
+		return infos;
+	}
+	
+	/**
+	 * 分享:使用系统自带的分享功能.(市场上有专门分享的sdk,一般不用这个)
+	 */
+	public static void share(Context context, String text) {
+		Intent intent = new Intent();
+		intent.setAction("android.intent.action.SEND");
+		intent.addCategory(Intent.CATEGORY_DEFAULT);
+		intent.setType("text/plain");
+		intent.putExtra(Intent.EXTRA_TEXT, text);
+		context.startActivity(intent);
+	}
+	
+	/**
+	 * 创建快捷图标(每次调用都会创建)
+	 * 权限:"com.android.launcher.permission.INSTALL_SHORTCUT"
+	 * @param appName app名称
+	 * @param className 启动界面的全类名("com.example.appdemo.MainActivity")
+	 * @param icon app位图图标(R.drawable.ic_launcher)
+	 */
+	public static void installShortCut(Context context,String appName,String className, int icon) {
+		Intent intent = new Intent();
+		intent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+		intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, appName);
+		intent.putExtra(Intent.EXTRA_SHORTCUT_ICON, BitmapFactory.decodeResource(context.getResources(), icon));
+		Intent shortcutIntent = new Intent();
+		shortcutIntent.setAction("android.intent.action.MAIN");
+		shortcutIntent.addCategory("android.intent.category.LAUNCHER");
+		shortcutIntent.setClassName(context.getPackageName(), className);
+		intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+		context.sendBroadcast(intent);
+	}
+	
+	/**
+	 * 获取正在运行的进程
+	 * @param context 上下文
+	 * @return RunningAppProcessInfo的集合
+	 */
+	public static List<RunningAppProcessInfo> getRunningProcess(Context context){
+		ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+		List<RunningAppProcessInfo> infos = am.getRunningAppProcesses();
+		return infos;
+	}
+	
+	/**
+	 * 获取手机的可用内存
+	 * @param context 上下文
+	 * @return 可用空间
+	 */
+	public static long getAvailMem(Context context){
+		ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+		MemoryInfo outInfo = new MemoryInfo();
+		am.getMemoryInfo(outInfo);
+		return outInfo.availMem;
+	}
+	
+	/**
+	 * 获取手机的总内存
+	 * @param context 上下文
+	 * @return long 总空间 B/0
+	 */
+	@SuppressLint("NewApi")
+	public static long getTotalMem(Context context){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+    		// apk16才可获取
+    		ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+    		MemoryInfo outInfo = new MemoryInfo();
+    		am.getMemoryInfo(outInfo);
+    		return outInfo.totalMem;
+        }else{
+    		try {
+				File file = new File("/proc/meminfo");
+				FileInputStream fis = new FileInputStream(file);
+				BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+				String line = br.readLine();
+				//MemTotal:         513000 kB
+				StringBuilder sb = new StringBuilder();
+				for(char c: line.toCharArray()){
+					if(c>='0'&&c<='9'){
+						sb.append(c);
+					}
+				}
+				br.close();
+				return Long.parseLong(sb.toString())*1024;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return 0;
+			}
+        }
+	}
+	
+	/**
+	 * 获取所有的进程信息(耗时操作)
+	 * @param context 上下文
+	 * @return
+	 */
+	public static List<TaskInfo> getTaskInfos(Context context){
+		ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+		PackageManager pm = context.getPackageManager();
+		List<RunningAppProcessInfo> processInfos = am.getRunningAppProcesses();
+		List<TaskInfo> taskInfos = new ArrayList<TaskInfo>();
+		for(RunningAppProcessInfo processInfo : processInfos){
+			TaskInfo taskInfo = new TaskInfo();
+			//应用程序的包名。
+			String packname = processInfo.processName;
+			taskInfo.packname = packname;
+			android.os.Debug.MemoryInfo[] memoryInfos = am.getProcessMemoryInfo(new int[]{processInfo.pid});
+			long memsize = memoryInfos[0].getTotalPrivateDirty()*1024l;
+			taskInfo.memsize = memsize;
+			try {
+				ApplicationInfo applicationInfo = pm.getApplicationInfo(packname, 0);
+				Drawable icon = applicationInfo.loadIcon(pm);
+				taskInfo.icon = icon;
+				String name = applicationInfo.loadLabel(pm).toString();
+				taskInfo.name = name;
+				if((applicationInfo.flags&ApplicationInfo.FLAG_SYSTEM) == 0){
+					//用户进程
+					taskInfo.userTask = true;
+				}else{
+					//系统进程
+					taskInfo.userTask = false;
+				}
+			} catch (NameNotFoundException e) {
+				e.printStackTrace();
+				taskInfo.icon = (context.getResources().getDrawable(R.drawable.ic_default));
+				taskInfo.name = packname;
+			}
+			taskInfos.add(taskInfo);
+		}
+		return taskInfos;
+	}
+	
+	public static class TaskInfo{
+		public Drawable icon;
+		public String name;
+		public String packname;
+		public long memsize; //占用内存大小
+		public boolean userTask; //是否是用户进行
+	}
+	
+	/**
+	 * 终止其他进程(不可终止自身)
+	 * 权限:"android.permission.KILL_BACKGROUND_PROCESSES"
+	 * @param packName 包名("com.example.demo") 
+	 */
+	public static void stopTask(Context context,String packName) {
+		ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+		am.killBackgroundProcesses(packName);
 	}
 }

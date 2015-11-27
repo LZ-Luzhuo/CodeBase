daf38c846fbe504af7fdeac036643f400669648d
LZ-Luzhuo <luzhuo@vip.qq.com>
2015-11-27 22:13:42 +0800

    supplement demo code.

--- CodeBase/Case/Animation/View/ViewAnimationDemo.java
+++ CodeBase/Case/Animation/View/ViewAnimationDemo.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.appdemo.R;
@@ -44,6 +46,7 @@ public class ViewAnimationDemo extends Activity implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
+		// 布局动画 (摇头动画)
 		Animation animation = AnimationUtils.loadAnimation(this, R.anim.shake);//这是一个来回摇头的动画
 		//animation.setInterpolator(new Interpolator() {
 		//	@Override
@@ -53,5 +56,12 @@ public class ViewAnimationDemo extends Activity implements OnClickListener {
 		//	}
 		//}); //自定义动画
 		textview.startAnimation(animation);
+		
+		
+		// 代码动画 (旋转动画)
+		RotateAnimation ra = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+		ra.setDuration(1000);
+		ra.setRepeatCount(Animation.INFINITE); //不停的转
+		textview.startAnimation(ra);
 	}
 }

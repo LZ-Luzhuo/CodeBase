b7b6a8d6389a335e391cb20b76242626f2f798dc
LZ-Luzhuo <luzhuo@vip.qq.com>
2015-12-02 14:57:31 +0800

    add method to ToastUtil.java.

--- CodeBase/utils/ToastUtil.java
+++ CodeBase/utils/ToastUtil.java
@@ -80,4 +80,17 @@ public class ToastUtil {
 		toast.setView(view);
 		toast.show();
 	}
+	
+	private static Toast quickToast;
+	/**
+	 * 快速显示的吐司
+	 * @param mContext
+	 * @param msg
+	 */
+	public static void showQuickToast(Context context, String msg) {
+		if(quickToast == null)
+			quickToast = Toast.makeText(context, "", Toast.LENGTH_SHORT);
+		quickToast.setText(msg);
+		quickToast.show();
+	}
 }
